02718e4fc99bcfac104d9f42855cd432989df703
ivanz <xvanx84@gmail.com>
2024-03-04 01:12:21 +0100

    try auth_identity

--- task_tracker/app/controllers/auth_identities_controller.rb
+++ task_tracker/app/controllers/auth_identities_controller.rb
@@ -1,6 +1,5 @@
 class AuthIdentitiesController < ApplicationController
   def create
-    binding.pry
     auth_identity = AuthIdentity.find_or_create_with_omniauth(auth_hash)
     if auth_identity.persisted?
       sign_in_and_redirect(auth_identity.user)
